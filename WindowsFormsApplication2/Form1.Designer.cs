9b80cd611bdcae9d18852db3a6d6a5607bcfe3a9
chrisli <euwiqo4487@gmail.com>
2019-03-04 00:20:14 +0800

    virst version commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReadStream = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ReadStream
+            // 
+            this.ReadStream.Location = new System.Drawing.Point(1020, 12);
+            this.ReadStream.Name = "ReadStream";
+            this.ReadStream.Size = new System.Drawing.Size(75, 23);
+            this.ReadStream.TabIndex = 0;
+            this.ReadStream.Text = "ReadStream";
+            this.ReadStream.UseVisualStyleBackColor = true;
+            this.ReadStream.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(989, 574);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1101, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(1020, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "monitor";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(1020, 124);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(305, 462);
+            this.rtbLog.TabIndex = 4;
+            this.rtbLog.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1112, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(24, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(1018, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "圖片變動達";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(1140, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "% 發送通知";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 598);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ReadStream);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReadStream;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MjpegProcessor;
+
+namespace WindowsFormsApplication2
+{
+    
+    public partial class Form1 : Form
+    {
+        #region declarations
+        Label lb = new Label();
+        Label lb1 = new Label();
+        Label lb2 = new Label();
+        Label lb3 = new Label();
+        bool isDrag = false;
+        int px = 10;
+        int py = 10;
+        //Label lb4 = new Label();
+        //Label lb5 = new Label();
+        //Label lb6 = new Label();
+        //Label lb7 = new Label();
+        bool isMonitor = false;
+        MjpegDecoder mjp;
+        #endregion
+
+        #region Property
+        #endregion
+
+        #region MemberFuntion
+        public Form1()
+        {
+            InitializeComponent();
+            mjp = new MjpegDecoder();
+            mjp.FrameReady += mjp_ready;
+        }
+        void mjp_ready(object sender, FrameReadyEventArgs e)
+        {
+            pictureBox1.Image = e.Bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mjp.ParseStream(new Uri("http://rr114.ddns.net:8080/?action=stream"));
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button1.Enabled = false;
+            pictureBox1.Controls.Clear();
+
+            lb.BackColor = Color.Red;
+            lb.Width = 10;
+            lb.Top = 10;
+            lb.Left = 10;
+            lb.Height = 10;
+            lb.MouseDown += MouseDown;
+            lb.MouseUp += MouseUp;
+            lb.MouseMove += MouseMove;
+            pictureBox1.Controls.Add(lb);
+
+            lb1.BackColor = Color.Red;
+            lb1.Width = 10;
+            lb1.Height = 10;
+            lb1.Top = 10;
+            lb1.Left = 80;
+            lb1.MouseDown += MouseDown;
+            lb1.MouseUp += MouseUp;
+            lb1.MouseMove += MouseMove;
+            pictureBox1.Controls.Add(lb1);
+
+            lb2.BackColor = Color.Red;
+            lb2.Width = 10;
+            lb2.Height = 10;
+            lb2.Top = 80;
+            lb2.Left = 10;
+            lb2.MouseDown += MouseDown;
+            lb2.MouseUp += MouseUp;
+            lb2.MouseMove += MouseMove;
+            pictureBox1.Controls.Add(lb2);
+
+            lb3.BackColor = Color.Red;
+            lb3.Width = 10;
+            lb3.Height = 10;
+            lb3.Top = 80;
+            lb3.Left = 80;
+            lb3.MouseDown += MouseDown;
+            lb3.MouseUp += MouseUp;
+            lb3.MouseMove += MouseMove;
+            pictureBox1.Controls.Add(lb3);
+        }
+
+        private void MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrag = true;
+            px = e.X;
+            py = e.Y;
+        }
+        private void MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrag = false;
+        }
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrag)
+            {
+                (sender as Label).Left += (e.X - px);
+                (sender as Label).Top += (e.Y - py);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((sender as Button).Text.Equals("monitor"))
+            {
+                lb.BackColor = lb1.BackColor = lb2.BackColor = lb3.BackColor = Color.Green;
+                isMonitor = true;
+                button2.Text = "Cancel";
+                button1.Enabled = false;
+                ReadStream.Enabled = false;
+                this.log("開始監控");
+            }
+            else if ((sender as Button).Text.Equals("Cancel"))
+            {
+                lb.BackColor = lb1.BackColor = lb2.BackColor = lb3.BackColor = Color.Red;
+                isMonitor = false;
+                button2.Text = "monitor";
+                ReadStream.Enabled = true;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (isMonitor)
+            {
+                float[] dashValue = { 2, 2 };
+                Pen blackPen = new Pen(Color.Green, 3);
+                blackPen.DashPattern = dashValue;
+                e.Graphics.DrawLine(blackPen, new Point(lb.Left, lb.Top), new Point(lb1.Left + 8, lb1.Top));
+                e.Graphics.DrawLine(blackPen, new Point(lb.Left, lb.Top), new Point(lb2.Left, lb2.Top + 8));
+                e.Graphics.DrawLine(blackPen, new Point(lb1.Left + 8, lb1.Top), new Point(lb3.Left + 8, lb3.Top + 8));
+                e.Graphics.DrawLine(blackPen, new Point(lb2.Left, lb2.Top + 8), new Point(lb3.Left, lb3.Top + 8));
+            }
+            else
+            {
+                float[] dashValue = { 2, 2 };
+                Pen blackPen = new Pen(Color.Red, 3);
+                blackPen.DashPattern = dashValue;
+                e.Graphics.DrawLine(blackPen, new Point(lb.Left, lb.Top), new Point(lb1.Left + 8, lb1.Top));
+                e.Graphics.DrawLine(blackPen, new Point(lb.Left, lb.Top), new Point(lb2.Left, lb2.Top + 8));
+                e.Graphics.DrawLine(blackPen, new Point(lb1.Left + 8, lb1.Top), new Point(lb3.Left + 8, lb3.Top + 8));
+                e.Graphics.DrawLine(blackPen, new Point(lb2.Left, lb2.Top + 8), new Point(lb3.Left, lb3.Top + 8));
+            }
+        }
+        public void log(string content)
+        {
+            rtbLog.AppendText(String.Format("{0} : {1}", content, DateTime.Now.TimeOfDay));
+        }
+        #endregion
+    }
+}
